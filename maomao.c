0517b88a00239d0fa726a6f7d164e3facbd6812a
DreamMaoMao <2523610504@qq.com>
2025-03-09 14:08:47 +0800

    浮动窗口小修复
    sdf

--- maomao.c
+++ maomao.c
@@ -4503,8 +4503,8 @@ setfloating(Client *c, int floating) {
 
   target_box = c->geom;
 
-  if (c->istiled) {
-    if (!c->swallowing && !c->is_open_animation) {
+  if (c->isfloating) {
+    if (c->istiled && !c->swallowing && !c->is_open_animation) {
       target_box.height = target_box.height * 0.8;
       target_box.width = target_box.width * 0.8;
     }
