763f52316624c911f7da87d9a36263b8607aa970
DreamMaoMao <2523610504@qq.com>
2025-03-25 10:36:53 +0800

    opt:warp cursor to new mon after foucsmon

--- maomao.c
+++ maomao.c
@@ -3228,6 +3228,7 @@ void focusmon(const Arg *arg) {
       selmon = dirtomon(arg->i);
     while (!selmon->wlr_output->enabled && i++ < nmons);
   }
+  warp_cursor_to_selmon(selmon);
   focusclient(focustop(selmon), 1);
 }
 
